90be675b46263bceefb25cc4f58eb9e77ea140b0
Nir Galon <nir@galons.io>
2024-11-10 19:32:09 +0200

    fix: header border color

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -41,7 +41,7 @@ nuxtApp.hooks.hookOnce("page:finish", () => {
 </script>
 
 <template>
-  <UHeader :links="links" class="dark:bg-gray-950">
+  <UHeader :links="links" class="dark:bg-gray-950 dark:border-gray-800">
     <template #logo>
       <span class="flex items-center gap-1">
         <UIcon name="i-heroicons-arrow-path-rounded-square" class="h-6 w-6" />
