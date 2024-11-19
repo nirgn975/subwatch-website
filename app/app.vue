40213719cfa563899fa949e01bacba50657fa3f3
Nir Galon <nir@galons.io>
2024-11-19 16:48:59 +0200

    fix: more meta tags

--- app/app.vue
+++ app/app.vue
@@ -8,9 +8,16 @@ useHead({
 });
 
 useSeoMeta({
+  title: "Subwatch",
+  ogTitle: "Subwatch",
+  description: "Turn subscription chaos into financial harmony.",
+  ogDescription: "Turn subscription chaos into financial harmony.",
   ogImage: "/social-card.jpg",
   twitterImage: "/social-card.jpg",
   twitterCard: "summary_large_image",
+  ogImageType: "image/jpeg",
+  ogUrl: "https://subwatch.co",
+  twitterCreator: "nirgn975",
 });
 </script>
 
