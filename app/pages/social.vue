fcc2613ffc270068dd3f83fd435aa8fb6f4656ae
Nir Galon <nir@galons.io>
2024-12-04 11:32:36 +0200

    fix: change social

--- app/pages/social.vue
+++ app/pages/social.vue
@@ -1,84 +1,83 @@
 <template>
-  <div class="max-w-6xl mx-auto my-20 flex gap-2 p-20 overflow-hidden">
+  <div class="max-w-7xl mx-auto my-20 flex gap-4 p-20 overflow-hidden">
     <div>
-      <span class="flex items-center gap-1 mb-2 text-4xl">
-        <UIcon name="i-heroicons-arrow-path-rounded-square" class="h-10 w-10" />
+      <span class="flex items-center gap-1 mb-2 text-5xl">
+        <UIcon name="i-heroicons-arrow-path-rounded-square" class="h-11 w-11" />
         SubWatch
       </span>
-      <p class="mb-12 text-2xl">Turn subscription chaos into financial harmony</p>
+      <p class="mb-12 text-3xl">Turn subscription chaos into financial harmony</p>
 
-      <ul class="space-y-4">
-        <li class="flex items-center gap-4 text-lg">
-          <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-9"><UIcon name="i-heroicons-sparkles" class="h-5 w-5" /></div>
+      <ul class="space-y-4 text-2xl">
+        <li class="flex items-center gap-4">
+          <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-10"><UIcon name="i-heroicons-sparkles" class="h-6 w-6" /></div>
           Track all your subscriptions in one place.
         </li>
-        <li class="flex items-center gap-4 text-lg">
-          <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-9"><UIcon name="i-heroicons-bell-alert" class="h-5 w-5" /></div>
+        <li class="flex items-center gap-4">
+          <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-10"><UIcon name="i-heroicons-bell-alert" class="h-6 w-6" /></div>
           Get notified before renewals.
         </li>
-        <li class="flex items-center gap-4 text-lg">
-          <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-9"><UIcon name="i-heroicons-currency-dollar" class="h-5 w-5" /></div>
+        <li class="flex items-center gap-4">
+          <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-10"><UIcon name="i-heroicons-currency-dollar" class="h-6 w-6" /></div>
           Maximize savings, minimize hassles, with zero effort.
         </li>
       </ul>
     </div>
 
     <div class="relative h-full w-full">
-      <img src="/assets/social-2.png" class="absolute -top-20 -right-36 h-[28rem] w-[28rem]" />
-      <img src="/assets/social-1.png" class="absolute top-0 left-2 h-[28rem] w-[28rem]" />
-      <img src="/assets/chrome-notification.png" class="absolute -top-8 left-44 h-[6rem] w-[20rem]" />
+      <img src="/assets/social-2.png" class="absolute -top-20 -right-36 h-[30rem] w-[30rem]" />
+      <img src="/assets/social-1.png" class="absolute top-0 -left-2 h-[30rem] w-[30rem]" />
+      <img src="/assets/chrome-notification.png" class="absolute -top-8 left-44 h-[7rem] w-[24rem]" />
     </div>
   </div>
 
   <div class="max-w-6xl mx-auto overflow-hidden my-60 p-2 gap-20 flex justify-center items-center">
-    <div class="text-3xl space-y-1">
-      <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-10 w-10 flex items-center justify-center">
-        <UIcon name="i-heroicons-sparkles" class="h-6 w-6" />
+    <div class="text-4xl space-y-1">
+      <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-12 w-12 flex items-center justify-center">
+        <UIcon name="i-heroicons-sparkles" class="h-8 w-8" />
       </div>
       <p>Understand your whole subscription chaos at a glance.</p>
     </div>
 
     <div class="relative h-full w-full">
-      <img src="/assets/social-1.png" class="h-[34rem] w-[34rem]" />
+      <img src="/assets/social-1.png" class="h-[40rem] w-[40rem]" />
     </div>
   </div>
 
-  <div class="max-w-6xl mx-auto overflow-hidden my-60 p-2 gap-4 flex justify-center items-center">
-    <div class="text-2xl space-y-1">
-      <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-10 w-10 flex items-center justify-center">
-        <UIcon name="i-heroicons-currency-dollar" class="h-6 w-6" />
+  <div class="max-w-7xl mx-auto overflow-hidden my-60 p-2 gap-2 flex justify-center items-center">
+    <div class="text-4xl space-y-1">
+      <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-12 w-12 flex items-center justify-center">
+        <UIcon name="i-heroicons-currency-dollar" class="h-8 w-8" />
       </div>
-      <p class="w-[30rem]">
-        Get the right insights to make a decision.<br />From montly distribution, alerts before<br />renewal, and the information you need in a tightly
-        beautiful way.
+      <p class="w-[36rem]">
+        Get the right insights to make a decision. From montly distribution, alerts before renewal, and the information you need in a tightly beautiful way.
       </p>
     </div>
     <div class="relative h-full w-full">
-      <img src="/assets/social-2.png" class="h-[30rem] w-[30rem]" />
+      <img src="/assets/social-2.png" class="h-[40rem] w-[40rem]" />
     </div>
   </div>
 
-  <div class="max-w-6xl mx-auto overflow-hidden my-96 p-2 gap-14 flex justify-center items-center">
-    <div class="text-2xl space-y-1">
-      <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-10 w-10 flex items-center justify-center">
-        <UIcon name="i-heroicons-bell-alert" class="h-6 w-6" />
+  <div class="max-w-7xl mx-auto overflow-hidden my-96 p-2 gap-14 flex justify-center items-center">
+    <div class="text-4xl space-y-1">
+      <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-12 w-12 flex items-center justify-center">
+        <UIcon name="i-heroicons-bell-alert" class="h-8 w-8" />
       </div>
       <p>Get notified before renewals. Maximize savings, minimize hassles, with zero effort.</p>
     </div>
     <div class="relative h-full w-full">
-      <img src="/assets/chrome-notification.png" class="h-[8rem] w-[28rem]" />
+      <img src="/assets/chrome-notification.png" class="h-[10rem] w-[35rem]" />
     </div>
   </div>
 
-  <div class="max-w-6xl mx-auto overflow-hidden my-96 p-2 gap-14 flex justify-center items-center">
-    <div class="text-2xl space-y-1">
-      <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-10 w-10 flex items-center justify-center">
-        <UIcon name="i-heroicons-cursor-arrow-rays" class="h-6 w-6" />
+  <div class="max-w-6xl mx-auto overflow-hidden my-96 p-2 gap-20 flex justify-center items-center">
+    <div class="text-3xl space-y-1">
+      <div class="p-2 ring-1 ring-neutral-700 rounded-lg h-12 w-12 flex items-center justify-center">
+        <UIcon name="i-heroicons-cursor-arrow-rays" class="h-8 w-8" />
       </div>
       <p>Add new subscriptions with just 2 clicks right from your Gmail.</p>
     </div>
     <div class="relative h-full w-full">
-      <img src="/assets/gmail.png" class="rounded-xl h-[15rem] w-[20rem]" />
+      <img src="/assets/gmail.png" class="rounded-xl h-[16rem] w-[22rem]" />
     </div>
   </div>
 </template>
