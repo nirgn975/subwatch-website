6367463b6b80aba676be263223bc5017a1970434
Nir Galon <nir@galons.io>
2025-03-03 12:41:27 +0200

    fix: update posthog and fix pageleave

--- app/plugins/posthog.client.js
+++ app/plugins/posthog.client.js
@@ -4,7 +4,7 @@ export default defineNuxtPlugin((nuxtApp) => {
   const runtimeConfig = useRuntimeConfig();
   const posthogClient = posthog.init(runtimeConfig.public.posthogPublicKey, {
     api_host: runtimeConfig.public.posthogHost,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
+    person_profiles: "always", // or 'always' to create profiles for anonymous users as well
     capture_pageview: false, // we add manual pageview capturing below
     loaded: (posthog) => {
       if (import.meta.env.MODE === "development") posthog.debug();
@@ -13,8 +13,14 @@ export default defineNuxtPlugin((nuxtApp) => {
 
   // Make sure that pageviews are captured with each route change
   const router = useRouter();
-  router.afterEach((to) => {
+  router.afterEach((to, from) => {
     nextTick(() => {
+      if (from.fullPath !== to.fullPath) {
+        posthog.capture("$pageleave", {
+          current_url: from.fullPath,
+        });
+      }
+
       posthog.capture("$pageview", {
         current_url: to.fullPath,
       });
